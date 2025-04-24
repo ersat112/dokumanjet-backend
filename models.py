84e071661a98ca5c98d8e2bfa435fe76e3addadd
ersat112 <ersat.bulbul@gmail.com>
2025-04-25 01:01:31 +0300

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String
-from database import Base # Base burada dışarıdan gelir
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
 
 class User(Base):
 __tablename__ = "users"
@@ -7,3 +7,10 @@ __tablename__ = "users"
 id = Column(Integer, primary_key=True, index=True)
 email = Column(String, unique=True, index=True, nullable=False)
 hashed_password = Column(String, nullable=False)
+
+class Favorite(Base):
+__tablename__ = "favorites"
+
+id = Column(Integer, primary_key=True, index=True)
+keyword = Column(String, nullable=False)
+user_email = Column(String, ForeignKey("users.email"), nullable=False)
